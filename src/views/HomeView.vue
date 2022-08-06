08d0df0e20605c5db8bc182ecbbf9de94ca605c5
Jong-Hwi Lee <paperlee@thedotio.com>
2022-08-06 15:10:32 +0900

    style. textarea

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,12 @@
     <div>
       <thread-item v-for="(i) in 6" :key="`thread-${i}`" />
     </div>
+
+    <div class="input-group">
+      <label for="content">
+        <textarea id="content"></textarea>
+      </label>
+    </div>
   </u-i-layout>
 </template>
 
@@ -12,3 +18,20 @@ import UILayout from '@/components/UILayout.vue';
 import ChannelHeader from '@/components/channel/ChannelHeader.vue';
 import ThreadItem from '@/components/thread/ThreadItem.vue';
 </script>
+
+<style lang="scss" scoped>
+.input-group {
+  position:fixed;
+  bottom:0;
+  width:100%;
+  padding:12px;
+  border-top:1px solid #ddd;
+  box-sizing: border-box;
+
+  textarea {
+    border:1px solid #ddd;
+    border-radius:4px;
+    width:100%;
+  }
+}
+</style>
